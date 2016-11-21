002c74a2af4667ef140df9eb36b9d741e06c3b40
GeoAndr <tzwrtzina2711994@gmail.com>
2016-11-21 20:33:26 +0200

    changed

--- Ergasia1/codeworks/distances.h
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-#include <inttypes.h>
-
-int distance_Hamming(uint64_t, uint64_t);
-double distance_Euclidean(double *, double *, int);
-double distance_Cosine(double *, double *, int);
